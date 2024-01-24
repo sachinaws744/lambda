4da89ff119272c09d0645f92341939bf83a5d1da
sachinaws744 <152378088+sachinaws744@users.noreply.github.com>
2024-01-24 09:46:25 +0000

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,11 @@
+import json
+import boto3
+def lambda_handler(event, context):
+    client = boto3.client('ec2', region_name= 'ap-south-1')
+    response = client.run_instances(
+           ImageId='ami-00952f27cf14db9cd',
+           InstanceType='t2.micro',
+           KeyName='terraform_mumbai',
+           MaxCount=1,
+           MinCount=1
+           )
